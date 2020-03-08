c73ec85d901e9f6db985bf254a509278827ba279
Swadesh Behera <itswadesh@gmail.com>
2020-03-08 22:51:24 +0530

    components

--- components/footer/StickyFooter.vue
+++ components/footer/StickyFooter.vue
@@ -3,26 +3,22 @@
     class="mt-2 shadow-lg w-full bg-gray-100 border-t overflow-x-auto fixed bottom-0 z-10 text-gray-600"
   >
     <div class="flex justify-around items-center">
-      <nuxt-link to="/" class="category-item">
-        <i class="fa fa-tag" />
-        <span class="text-xs">Home</span>
-      </nuxt-link>
-      <nuxt-link to="/pickup" class="category-item">
-        <i class="fa fa-tag" />
-        <span class="text-xs">Pickup</span>
-      </nuxt-link>
-      <nuxt-link to="/delivery" class="category-item">
+      <nuxt-link to="/search" class="category-item" active-class="text-primary">
         <i class="fa fa-search" />
-        <span class="text-xs">Delivery</span>
+        <span class="text-xs">Dishes</span>
       </nuxt-link>
-      <nuxt-link to="/my" class="category-item">
+      <nuxt-link to="/my" class="category-item" active-class="text-primary">
         <i class="fa fa-user" />
-        <span class="text-xs">Profile</span>
+        <span class="text-xs">Account</span>
       </nuxt-link>
-      <!-- <nuxt-link to="/reviews" class="category-item">
+      <nuxt-link to="/orders" class="category-item" active-class="text-primary">
+        <i class="fa fa-tag" />
+        <span class="text-xs">Orders</span>
+      </nuxt-link>
+      <nuxt-link to="/reviews" class="category-item" active-class="text-primary">
         <i class="fa fa-shopping-cart" />
         <span class="text-xs">Reviews</span>
-      </nuxt-link>-->
+      </nuxt-link>
     </div>
   </div>
 </template>
