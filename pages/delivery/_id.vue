18dc8e906474f93e2004ef28d1303a40febee629
Swadesh Behera <itswadesh@gmail.com>
2020-03-20 20:42:14 +0530

    delivery statuses

--- pages/delivery/_id.vue
+++ pages/delivery/_id.vue
@@ -3,8 +3,22 @@
     <nuxt-link to="/delivery">
       <i class="fa fa-long-arrow-left" />
     </nuxt-link>
-    <h3>Customer Details</h3>
+    <h3>Order Details</h3>
     <div v-if="order">
+      <div class="flex justify-between">
+        <div>{{order.payment.method}}</div>
+        <div class="text-2xl text-green-500">{{order.payment.amount/100 | currency}}</div>
+        <div>{{order.payment.status}}</div>
+      </div>
+      <div v-if="order.payment.status=='captured'" class="flex justify-between">
+        <div>{{order.payment.amount/100 | currency}}</div>
+        <!-- <Textbox v-model="cod.amount" label="COD Amount" /> -->
+        <div class="flex">
+          <Textbox v-model="order.cod_paid" label="COD Paid" />
+          <button @click="collectPayment(order.id,order.cod_paid)" class="primary px-4 py-2">Save</button>
+        </div>
+        <div>Balance: {{order.payment.amount/100 - +order.cod_paid | currency}}</div>
+      </div>
       <div v-if="order.address">Name: {{ order.address.firstName }} {{ order.address.lastName }}</div>
       <div>Phone: {{ order.user.phone }}</div>
       <div v-if="order.address">Address: {{ order.address.address }}</div>
@@ -25,51 +39,51 @@
           {{ i.vendor.restaurant }}
           {{ i.vendor.address.address }}
         </div>
-      </div>
-
-      <div class="flex flex-center">
-        <button
-          class="button"
-          v-if="order.status == 'Order Placed'"
-          @click="save(order._id, 'Delivered')"
-        >
-          <span class="fonttype grn">Delivered</span>
-        </button>
-        <button
-          class="button"
-          v-if="order.status !== 'Cancelled'"
-          @click="save(order._id, 'Cancelled')"
-        >
-          <span class="fonttype grn">Cancelled</span>
-        </button>
-        <button
-          v-if="order.status == 'Delivered'"
-          class="button"
-          @click="save(order._id, 'Order Placed')"
-        >
-          <span class="fonttype grn">Returned</span>
-        </button>
-        <button
-          v-if="order.status == 'Cancelled'"
-          class="button"
-          @click="save(order._id, 'Order Placed')"
-        >
-          <span class="fonttype grn">Put Back</span>
-        </button>
-        <button
-          v-if="order.status == 'Out For Delivery'"
-          class="button"
-          @click="save(order._id, 'Delivered')"
-        >
-          <span class="fonttype grn">Delivered</span>
-        </button>
-        <button
-          v-if="order.status == 'Out For Delivery'"
-          class="button"
-          @click="save(order._id, 'Order Placed')"
-        >
-          <span class="fonttype grn">Put Back</span>
-        </button>
+        <div>{{i.status}}</div>
+        <div class="flex flex-center">
+          <button
+            class="button"
+            v-if="i.status == 'Waiting for confirmation'"
+            @click="save(order.id, i.pid, 'Delivered')"
+          >
+            <span class="fonttype grn">Delivered</span>
+          </button>
+          <button
+            class="text-secondary mx-4 my-2"
+            v-if="i.status !== 'Cancelled'"
+            @click="save(order.id, i.pid, 'Cancelled')"
+          >
+            <span class>Cancelled</span>
+          </button>
+          <button
+            v-if="i.status == 'Delivered'"
+            class="button"
+            @click="save(order.id, i.pid, 'Waiting for confirmation')"
+          >
+            <span class="fonttype grn">Returned</span>
+          </button>
+          <button
+            v-if="i.status == 'Cancelled'"
+            class="button"
+            @click="save(order.id, i.pid, 'Waiting for confirmation')"
+          >
+            <span class="fonttype grn">Put Back</span>
+          </button>
+          <button
+            v-if="i.status == 'Out For Delivery'"
+            class="button"
+            @click="save(order.id, i.pid, 'Delivered')"
+          >
+            <span class="fonttype grn">Delivered</span>
+          </button>
+          <button
+            v-if="i.status == 'Out For Delivery'"
+            class="button"
+            @click="save(order.id, i.pid, 'Waiting for confirmation')"
+          >
+            <span class="fonttype grn">Put Back</span>
+          </button>
+        </div>
       </div>
     </div>
     <StickyFooter />
@@ -77,8 +91,11 @@
 </template>
 <script>
 const StickyFooter = () => import('~/components/footer/StickyFooter')
+const Textbox = () => import('~/components/ui/Textbox')
+const Heading = () => import('~/components/Heading')
 import order from '~/gql/order/order.gql'
 import updateOrder from '~/gql/order/updateOrder.gql'
+import collectPayment from '~/gql/order/collectPayment.gql'
 
 export default {
   middleware: ['isAuth'],
@@ -122,9 +139,24 @@ export default {
       } finally {
         this.$store.commit('busy', false)
       }
+    },
+    async collectPayment(id, cod_paid) {
+      try {
+        this.$store.commit('clearErr')
+        await this.$apollo.mutate({
+          mutation: collectPayment,
+          variables: { id, cod_paid: +cod_paid },
+          fetchPolicy: 'no-cache'
+        })
+        this.getData()
+      } catch (e) {
+        this.$store.commit('setErr', e, { root: true })
+      } finally {
+        this.$store.commit('busy', false)
+      }
     }
   },
-  components: { StickyFooter }
+  components: { StickyFooter, Textbox, Heading }
 }
 </script>
 <style scoped>
